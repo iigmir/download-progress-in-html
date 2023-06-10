c2eab6eb1ac34bce81a74c0d36178d55b38a61fd
iigmir <roc120j@gmail.com>
2023-06-11 00:07:41 +0800

    init

---
+++ help.md
@@ -0,0 +1,13 @@
+## Help
+
+Here are some references of progressbar:
+
+* [How to monitor the progress of a Javascript fetch - request and cancel it on demand.](https://dev.to/tqbit/how-to-monitor-the-progress-of-a-javascript-fetch-request-and-cancel-it-on-demand-107f)
+* [Upload progress indicators for fetch?](https://stackoverflow.com/a/35747208/7162445)
+* [Fetch API Download Progress Indicator?](https://stackoverflow.com/a/64123890)
+
+---
+
+By the way, I use `markdown-it` to render Markdown texts.
+
+* [markdown-it](https://github.com/markdown-it/markdown-it)
---
+++ index.js
@@ -0,0 +1,88 @@
+const dl_btn = document.querySelector("button[data-iapp-action='download']");
+const progressbar = document.querySelector("*[data-iapp-action='progressbar']");
+
+/**
+ * 
+ * @param {Blob} blob 
+ */
+const open_download_link = (blob) =>
+{
+    const a = document.createElement("a");
+    // Attributes
+    a.href = URL.createObjectURL(blob);
+    a.download = "download.mp3";
+    a.style.display = "none";
+    // *click* *click* *click*
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+};
+
+/**
+ * 
+ * @param {Number} loaded 
+ * @param {Number} total 
+ */
+const update_progressbar = (loaded, total) => {
+    progressbar.style.width = `${Math.round( loaded / total * 100)}%`;
+    progressbar.hidden = false;
+};
+
+/**
+ * @param {String} path 
+ */
+const open_download = (path = "/") =>
+{
+    const ajax = fetch(path);
+    ajax.then((response) =>
+    {
+        if (!response.ok)
+        {
+            throw new Error(`Failed to download MP3: ${url}`);
+        }
+        const get_total = (response) =>
+        {
+            const image_length = response.headers.get("content-length");
+            return parseInt(image_length, 10);
+        }
+        let downloaded = 0;
+        const res = new Response(new ReadableStream({
+            async start(controller) {
+                const total = get_total(response);
+                const reader = response.body.getReader();
+                for (; ;) {
+                    const { done, value } = await reader.read();
+                    if (done) {
+                        break;
+                    }
+                    downloaded += value.byteLength;
+                    update_progressbar(downloaded, total);
+                    controller.enqueue(value);
+                }
+                controller.close();
+            },
+        }));
+        return res.blob();
+    }).then(open_download_link).catch( up => { throw up });
+};
+
+dl_btn.addEventListener( "click", (event) =>
+{
+    const dom = event.target;
+    open_download(dom.dataset.iappDownloadpath);
+});
+
+const get_help  = document.querySelector("*[data-iapp-action='get-help']");
+const help_area = document.querySelector("*[data-iapp-render='get-help']");
+
+get_help.addEventListener("click", () => {
+    if( help_area.innerHTML.trim().length > 0 )
+    {
+        return;
+    }
+    const ajax = fetch("/help.md").then(r=>r.text());
+    ajax.then( (content) => {
+        help_area.innerHTML = window.markdownit().render( content );
+    });
+});
+
